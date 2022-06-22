89c669445573e6f17a8f3d5799a73ac57ebc7e50
Ariel Calderon <ariel.calderon@finantech.com.ec>
2022-06-22 12:21:03 -0500

    uno a muchos

---
+++ database/migrations/2022_06_22_154214_create_estudiantes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estudiantes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('edad');
+            $table->string('sexo');
+
+
+            $table->foreign('id')
+                    ->references('id')
+                    ->on('carreras')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estudiantes');
+    }
+};
